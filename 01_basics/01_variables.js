ad88ff9b8d692f23eb31326f8ad6eb6bb2a35f02
Taher Ali <129791115+TaherAli331108@users.noreply.github.com>
2025-04-02 01:17:00 +0000

    Container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "ta321108@gmail.com"
+var accountPassword = "12345"
+accountCity = "Guwahati"
+
+console.table([accountId, accountEmail, accountPassword])
+
+/*
+ In javascript prefer not to use variable because of issue in block scope and functional scope.
+
+*/
